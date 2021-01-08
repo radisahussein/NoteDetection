399e897f285385839c10316f07397aec6b96b1e5
radisahussein <radisahrachmadi@gmail.com>
2021-01-09 00:36:25 +0700

    final
    all functions working properly
    
    maybe need more commenting and design for UI menu

--- draft1.py
+++ draft1.py
@@ -1,8 +1,8 @@
 #-------------------------------------------------#
 '''
+
 FINAL PROJECT ANALYSIS OF ALGORITHM
 SEMESTER 3 2020-2021
-
 
 GROUP MEMBERS:
 
@@ -15,7 +15,8 @@ GROUP MEMBERS:
 
 import numpy as np
 import pyaudio
-
+import wave
+import os
 #---------------INITIALIZING VALUES---------------#
 
 # Initializing values for the range of the note detection
@@ -99,7 +100,7 @@ def liveDetection(lowestNote, highestNote, samplesPerFFT, samplingFrequency, sam
     #Hanning Window Function
     window = 0.5 * (1 - np.cos(np.linspace(0,2*np.pi, samplesPerFFT, False)))
 
-    print('Sampling Frequency:',samplingFrequency, "Hz")
+    print('\nSampling Frequency:',samplingFrequency, "Hz")
     print('Max Resolution:', frequencyStep,"Hz\n")
 
     for i in range(0,int(samplingFrequency / sampleFrameSize * duration)):
@@ -127,18 +128,16 @@ def liveDetection(lowestNote, highestNote, samplesPerFFT, samplingFrequency, sam
             if defaultNote not in getNoteName(n0):
                 print ('Frequency: {:7.2f} Hz   |   Note: {:>3s} {:+.2f}'.format(freq,getNoteName(n0), n-n0))
 
-    print("Finish Stream")
+    print("FINISH STREAM")
     stream.stop_stream()
     stream.close()
 
-def recordNotes(samplesPerFFT, samplingFrequency, sampleFrameSize, frequencyStep,duration):
+#Function for recording audio files
+def recordNotes(samplesPerFFT, samplingFrequency, sampleFrameSize, frequencyStep,duration,filename):
 
-    chunk = 1024
-    
-    channels = 2
-    fs = 44100
-    length = 5
-    filename = "soundsample.wav"
+    #Initialize Path
+    path = 'C:/Users/radis/OneDrive/Desktop/Github/Binus/Final Project/AoA SEM3/FinalProjectAA/recordings/'
+
 
     # Initializing pyaudio
     stream = pyaudio.PyAudio().open(format=pyaudio.paInt16,
@@ -152,13 +151,170 @@ def recordNotes(samplesPerFFT, samplingFrequency, sampleFrameSize, frequencyStep
     #Hanning Window Function
     window = 0.5 * (1 - np.cos(np.linspace(0,2*np.pi, samplesPerFFT, False)))
     
-    print("----------Now Recording----------")
+    print("----------Now Recording----------\n")
 
-    
+    frames = []
     for i in range (int(samplingFrequency / sampleFrameSize * duration)):
         data = stream.read(sampleFrameSize)
-        
+        frames.append(data)
+    
+    print("----------Finished Recording----------")
 
+    #End stream
+    stream.stop_stream()
+    stream.close()
+    
+    #Writing frames to filename
+    wf = wave.open(path+filename,"wb")
+    wf.setnchannels(1)
+    wf.setsampwidth(pyaudio.get_sample_size(pyaudio.paInt16))
+    wf.setframerate(samplingFrequency)
+    wf.writeframes(b"".join(frames))
+    wf.close()
+    
+#Function to view audio files recorded and saved by the user
+def history(lowestNote, highestNote, samplesPerFFT, samplingFrequency, sampleFrameSize, frequencyStep):
+    
+    #Initialize an array to store all the files
+    files = []
+
+    #Path to the audio files
+    path = 'C:/Users/radis/OneDrive/Desktop/Github/Binus/Final Project/AoA SEM3/FinalProjectAA/recordings/'
+
+    #Finding files with the .wav extension
+    for f in os.listdir(path):
+        if f.endswith('.wav'):
+            files.append(f)
+
+    #For menu
+    print("")
+    filecount = 1
+    for i in files:
+        print(filecount,")",i)
+        filecount += 1
+    
+    print("---------------OPEN FILE---------------")
+    filechoice = int(input("Select File (0 to exit this page): "))
+    print("---------------------------------------")
+
+    if filechoice < filecount+1:
+        if filechoice == 0:
+            mainProgram()
+
+        for i in files:
+            if files.index(i)+1 == filechoice:
+                selectFile(i,lowestNote, highestNote, samplesPerFFT, samplingFrequency, sampleFrameSize, frequencyStep)
+    else:
+        print("Selection doesn't exist!")
+        history()
+
+#Function to get notes and frequency of a saved audio file
+def analyzeFile(file,lowestNote, highestNote, samplesPerFFT, samplingFrequency, sampleFrameSize, frequencyStep):
+
+    #path
+    path = "C:/Users/radis/OneDrive/Desktop/Github/Binus/Final Project/AoA SEM3/FinalProjectAA/recordings/"
+
+    #read file
+    fileread = wave.open(path+file,'r')
+
+    # To get the min and max index in FFT of the notes
+    minIndex = max(0,int(np.floor(noteToFFT(lowestNote-1))))
+    maxIndex = min(samplesPerFFT, int(np.ceil(noteToFFT(highestNote+1))))
+
+    # Allocating space for FFT process
+    temp = np.zeros(samplesPerFFT, dtype=np.float32)
+    numberOfFrames = 0
+
+    #Hanning Window Function
+    window = 0.5 * (1 - np.cos(np.linspace(0,2*np.pi, samplesPerFFT, False)))
+
+    #get duration of file
+    duration = (fileread.getnframes() / fileread.getframerate())
+    print(duration)
+
+    print('Sampling Frequency:',samplingFrequency, "Hz")
+    print('Max Resolution:', frequencyStep,"Hz\n")
+
+    for i in range(0,int(samplingFrequency / sampleFrameSize * duration)):
+
+        # Shifting the data down and the new data in
+        temp[:-sampleFrameSize] = temp[sampleFrameSize:]
+        temp[-sampleFrameSize:] = np.frombuffer(fileread.readframes(sampleFrameSize), np.int16)
+
+        # Run the FFT on the windowed buffer
+        fft = np.fft.rfft(temp * window)
+
+        # Obtain frequency
+        freq = (np.abs(fft[minIndex:maxIndex]).argmax() + minIndex) * frequencyStep
+
+        #Get the note number and nearest note
+        n = frequencyToNumber(freq)
+        n0 = int(round(n))
+
+        numberOfFrames += 1
+
+        #Default note when no sound is played or heard by device
+        defaultNote = "A#2"
+
+        #Print note names and frequency
+        if numberOfFrames >= framesPerFFT:
+            if defaultNote not in getNoteName(n0):
+                print ('Frequency: {:7.2f} Hz   |   Note: {:>3s} {:+.2f}'.format(freq,getNoteName(n0), n-n0))
+    
+    selectFile(file,lowestNote, highestNote, samplesPerFFT, samplingFrequency, sampleFrameSize, frequencyStep)
+
+
+def selectFile(f,lowestNote, highestNote, samplesPerFFT, samplingFrequency, sampleFrameSize, frequencyStep):
+
+    #path
+    path = "C:/Users/radis/OneDrive/Desktop/Github/Binus/Final Project/AoA SEM3/FinalProjectAA/recordings/"
+
+    #Menu for selected audio file
+    print("\nFile",f,"selected:\n")
+    print("1. Play")
+    print("2. Delete")
+    print("3. Analyze")
+    print("4. Exit")
+    print("------------------------------")
+
+    selectChoice = int(input("What to do: "))
+
+    while True:
+        if selectChoice == 1:
+            
+            file = wave.open(path+f, 'rb')
+
+            p = pyaudio.PyAudio()
+
+            stream = p.open(format = pyaudio.paInt16,
+                            channels = 1, rate=samplingFrequency,
+                            output=True)
+            
+            data = file.readframes(1024)
+            
+            print("---------------PLAYING FILE---------------")
+
+            while data:
+                stream.write(data)
+                data = file.readframes(1024)
+            print("---------------END---------------")
+
+            stream.stop_stream()
+            stream.close()
+            p.terminate()
+            selectFile(f,lowestNote, highestNote, samplesPerFFT, samplingFrequency, sampleFrameSize, frequencyStep)
+
+        #Deletes the selected file 
+        if selectChoice == 2:
+            os.remove("C:/Users/radis/OneDrive/Desktop/Github/Binus/Final Project/AoA SEM3/FinalProjectAA/recordings/"+f)
+            print("\nFile Deleted!")
+            history(lowestNote, highestNote, samplesPerFFT, samplingFrequency, sampleFrameSize, frequencyStep)
+        
+        if selectChoice == 3:
+            analyzeFile(f,lowestNote, highestNote, samplesPerFFT, samplingFrequency, sampleFrameSize, frequencyStep)
+
+        if selectChoice == 4:
+            history(lowestNote, highestNote, samplesPerFFT, samplingFrequency, sampleFrameSize, frequencyStep)
 
 
 #---------------MAIN PROGRAM---------------#
@@ -182,12 +338,24 @@ def mainProgram():
             mainProgram()
 
         elif selection == '2':
-            duration = input("Record Duration: ")
-            recordNotes()
+            duration = int(input("Record Duration: "))
+            inputfile = input("Save File as: ")
+            filename = inputfile + '.wav'
+
+            if os.path.isfile("C:/Users/radis/OneDrive/Desktop/Github/Binus/Final Project/AoA SEM3/FinalProjectAA/recordings/"+filename):
+                print("File name already exists!")
+            else:
+                recordNotes(samplesPerFFT,samplingFrequency,sampleFrameSize,frequencyStep,duration,filename)
+
             mainProgram()
         
+        elif selection == '3':
+            history(lowestNote, highestNote, samplesPerFFT, samplingFrequency, sampleFrameSize, frequencyStep)
+            mainProgram()
+            
+
         elif selection == '4':
-            break
+            exit()
         
         else:
             print("\nSelection does not exist!\n")
